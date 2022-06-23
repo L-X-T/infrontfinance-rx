L-X-T/rx-smartcommerce

[NgRX] Add error message output

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -17,3 +17,4 @@ export const selectFlightsWithProps = (props: {
   createSelector(selectFlights, (flights) => flights.filter((f) => !props.blackList.includes(f.id)));
 
 export const selectIsLoadingFlights = createSelector(selectFlightBookingState, (s) => s.isLoadingFlights);
+export const selectIsLoadingError = createSelector(selectFlightBookingState, (s) => s.loadingError);

apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 import { FlightBookingAppState } from '../+state/flight-booking.reducer';
 import { loadFlights, updateFlight } from '../+state/flight-booking.actions';
 import { take } from 'rxjs/operators';
-import { selectFlightsWithProps, selectIsLoadingFlights } from '../+state/flight-booking.selectors';
+import { selectFlightsWithProps, selectIsLoadingError, selectIsLoadingFlights } from '../+state/flight-booking.selectors';
 
 @Component({
   selector: 'flight-search',
@@ -25,6 +25,7 @@ export class FlightSearchComponent implements OnInit {
 
   flights$ = this.store.select(selectFlightsWithProps({ blackList: [3] }));
   isLoadingFlight$ = this.store.select(selectIsLoadingFlights);
+  loadingError$ = this.store.select(selectIsLoadingError);
 
   constructor(private flightService: FlightService, private store: Store<FlightBookingAppState>) {}
 

L-X-T/infrontfinance-rx

apps/flight-app/src/app/flight-booking/+state/flight-booking.selectors.ts
@@ -17,3 +17,4 @@ export const selectFlightsWithProps = (props: {
   createSelector(selectFlights, (flights) => flights.filter((f) => !props.blackList.includes(f.id)));
 
 export const selectIsLoadingFlights = createSelector(selectFlightBookingState, (s) => s.isLoadingFlights);
+export const selectLoadingError = createSelector(selectFlightBookingState, (s) => s.loadingError);

apps/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -6,7 +6,7 @@ import { Store } from '@ngrx/store';
 import { FlightBookingAppState } from '../+state/flight-booking.reducer';
 import { loadFlights, updateFlight } from '../+state/flight-booking.actions';
 import { take } from 'rxjs/operators';
-import { selectFlightsWithProps, selectIsLoadingFlights } from '../+state/flight-booking.selectors';
+import { selectFlightsWithProps, selectLoadingError, selectIsLoadingFlights } from '../+state/flight-booking.selectors';
 
 @Component({
   selector: 'flight-search',
@@ -25,6 +25,7 @@ export class FlightSearchComponent implements OnInit {
 
   flights$ = this.store.select(selectFlightsWithProps({ blackList: [3] }));
   isLoadingFlight$ = this.store.select(selectIsLoadingFlights);
+  loadingError$ = this.store.select(selectLoadingError);
 
   constructor(private flightService: FlightService, private store: Store<FlightBookingAppState>) {}
 